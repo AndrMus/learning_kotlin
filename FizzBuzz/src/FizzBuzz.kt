15fdfd1c8b2bb723154de440f35c96ba5751a498
AndrMus <soonay13@yandex.ru>
2020-12-07 02:22:22 +0300

    new fizzbuzz

---
+++ FizzBuzz/src/FizzBuzz.kt
@@ -0,0 +1,16 @@
+fun fizz(n: Int): Array<String> {
+    var elements: Array<String> = emptyArray()
+    for (number in 1..n) {
+        if (number % 3 == 0) {
+            elements += "buzz"
+        } else {
+            elements += number.toString()
+        }
+    }
+    return elements
+}
+
+fun main() {
+    val x = fizz(12)
+    print(x.contentToString())
+}
