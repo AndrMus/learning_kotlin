be1149dacff5fe525db63a49cd3ff51385b381f2
AndrMus <soonay13@yandex.ru>
2020-12-07 03:31:37 +0300

    OvO

--- FizzBuzz/src/FizzBuzz.kt
+++ FizzBuzz/src/FizzBuzz.kt
@@ -1,7 +1,11 @@
 fun fizz(n: Int): Array<String> {
     var elements: Array<String> = emptyArray()
     for (number in 1..n) {
-        if (number % 3 == 0) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            elements += "fizzbuzz"
+        } else if (number % 3 == 0 && number % 5 != 0) {
+            elements += "fizz"
+        } else if (number % 3 != 0 && number % 5 == 0) {
             elements += "buzz"
         } else {
             elements += number.toString()
