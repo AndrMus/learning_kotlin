8326b1e4520792f2c278fa375e715efe92c786be
AndrMus <soonay13@yandex.ru>
2020-12-11 15:46:08 +0300

    less %

--- FizzBuzz/src/FizzBuzz.kt
+++ FizzBuzz/src/FizzBuzz.kt
@@ -1,20 +1,38 @@
 fun fizz(n: Int): Array<String> {
     var elements: Array<String> = emptyArray()
+    var v: Boolean = false
+    var t: Boolean = false
+
     for (number in 1..n) {
-        if (number % 3 == 0 && number % 5 == 0) {
-            elements += "fizzbuzz"
-        } else if (number % 3 == 0) {
-            elements += "fizz"
-        } else if (number % 5 == 0) {
+
+        if (number % 3 == 0) {
+            t = true
+        }
+        if (number % 5 == 0) {
+            v = true
+        }
+
+        if (t == true) {
+            if (v == true) {
+                elements += "fizzbuzz"
+            } else {
+                elements += "fizz"
+            }
+        } else if (v == true) {
             elements += "buzz"
         } else {
             elements += number.toString()
         }
+
+        t = false
+        v = false
     }
+
     return elements
+
 }
 
 fun main() {
-    val x = fizz(n = 30)
+    val x = fizz(n = 15)
     print(x.contentToString())
 }
